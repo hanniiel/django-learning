8230e5996b6c5168f855dc17f7208ef269078088
Nelson Manuel Austria Acosta <hanniiel@live.com.mx>
2021-08-15 14:14:09 -0500

    update

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Project
 
 # Create your views here.
 def projects(request):
-    msg = 'holis from bcknd'
-    number = 11
-    context = {'message':msg,'number':number}
+    projects = Project.objects.all() #works on view as well
+    context = {'projects' : projects}
     return render(request,'projects/projects.html',context)
 
 def project(request,pk):
-    return render(request,'projects/single-project.html')
+    project = Project.objects.get(id=pk)
+    tags = project.tags.all()
+    return render(request,'projects/single-project.html',{'project':project})
